2a9394872b6f04bf8dd1459b93939cca03d68750
irinaalmaeva <almaeva.irina@gmail.com>
2024-09-16 21:04:07 +0300

    Update settings.py

--- flower_delivery/flower_delivery/settings.py
+++ flower_delivery/flower_delivery/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'flowers',  # Приложение для товаров
+    'orders',   # Приложение для заказов
+    'bot',      # Приложение для Telegram бота
 ]
 
 MIDDLEWARE = [
@@ -103,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 

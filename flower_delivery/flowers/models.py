eca421a0ff920ba05a98e8958e63954312d65fc6
irinaalmaeva <almaeva.irina@gmail.com>
2024-09-16 23:55:57 +0300

    added flowers\models.py

--- flower_delivery/flowers/models.py
+++ flower_delivery/flowers/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Flower(models.Model):
+    name = models.CharField(max_length=100)  # Название цветка
+    description = models.TextField(blank=True)  # Описание цветка
+    price = models.DecimalField(max_digits=10, decimal_places=2)  # Цена цветка
+    image = models.ImageField(upload_to='flowers/', blank=True, null=True)  # Изображение цветка
+    created_at = models.DateTimeField(auto_now_add=True)  # Дата добавления цветка
+
+    def __str__(self):
+        return self.name
+
+
+

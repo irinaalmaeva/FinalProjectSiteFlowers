7685f8e45cad444e4d9f42c4a08d8fd8cdab536c
irinaalmaeva <almaeva.irina@gmail.com>
2024-09-17 22:02:12 +0300

    update oders \forms, models; add order_success, change checkout

--- flower_delivery/orders/forms.py
+++ flower_delivery/orders/forms.py
@@ -1,6 +1,9 @@
 from django import forms
-
-class OrderForm(forms.Form):
-    name = forms.CharField(label='Имя', max_length=100)
-    address = forms.CharField(label='Адрес', widget=forms.Textarea(attrs={'rows': 3}))
-    flowers = forms.CharField(label='Цветы', widget=forms.Textarea(attrs={'rows': 5}))
+from .models import Order
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ['address']  # Здесь мы включаем только адрес, так как цветок привязан в views
+        widgets = {
+            'address': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите адрес доставки'}),
+        }
--- flower_delivery/orders/models.py
+++ flower_delivery/orders/models.py
@@ -11,9 +11,11 @@ class OrderStatus(models.Model):
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
     flowers = models.ManyToManyField(Flower, verbose_name='Цветы')
-    status = models.ForeignKey(OrderStatus, on_delete=models.SET_NULL, null=True, verbose_name='Статус')
+    status = models.CharField(max_length=20, choices=[('new', 'Новый'), ('in_progress', 'В процессе'), ('delivered', 'Доставлен')], default='new', verbose_name='Статус')
+    address = models.CharField(max_length=255, verbose_name='Адрес доставки')
     order_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата заказа')
 
     def __str__(self):
-        return f"Заказ №{self.id} от {self.user.username}"
+        return f'Заказ №{self.id} - {self.flower.name}'
+
 

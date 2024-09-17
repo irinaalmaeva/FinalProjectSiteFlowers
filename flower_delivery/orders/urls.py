92405daa7d488808a9860de87be3a44949d454d1
irinaalmaeva <almaeva.irina@gmail.com>
2024-09-18 00:36:49 +0300

    Update urls.py

--- flower_delivery/orders/urls.py
+++ flower_delivery/orders/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('place_order/<int:flower_id>/', views.place_order, name='place_order'),
     path('order_history/', views.order_history, name='order_history'),
     path('order/success/', views.order_success, name='order_success'),
+    path('order_detail/<int:pk>/', views.order_detail, name='order_detail'),
 ]

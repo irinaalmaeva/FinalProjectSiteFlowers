dc23db52776478d9a9f3d7ba820ed1cd3e72cc62
irinaalmaeva <almaeva.irina@gmail.com>
2024-09-17 22:49:30 +0300

    added flowers\admin.py?

--- flower_delivery/flowers/urls.py
+++ flower_delivery/flowers/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.flower_catalog, name='flower_catalog'),
     path('checkout/', views.checkout, name='checkout'),
-    path('flower/<int:pk>/', views.flower_detail, name='flower_detail'),
+
 ]
 

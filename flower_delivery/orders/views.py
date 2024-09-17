75c3238be6dacd3edcf9d7452730d131cd45a25e
irinaalmaeva <almaeva.irina@gmail.com>
2024-09-17 21:33:23 +0300

    Update views.py

--- flower_delivery/orders/views.py
+++ flower_delivery/orders/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from flowers.models import Flower
 from .models import Order
+from .forms import OrderForm
+
 
 def order_history(request):
     orders = Order.objects.filter(user=request.user)
@@ -8,3 +12,23 @@ def order_history(request):
 def order_detail(request, pk):
     order = get_object_or_404(Order, pk=pk)
     return render(request, 'order_detail.html', {'order': order})
+
+
+def place_order(request, flower_id):
+    flower = get_object_or_404(Flower, id=flower_id)
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            order = form.save(commit=False)
+            order.flower = flower  # Привязываем цветок к заказу
+            order.user = request.user  # Привязываем текущего пользователя (если реализована система пользователей)
+            order.save()
+            messages.success(request, 'Ваш заказ успешно оформлен!')
+            return redirect('order_success')
+        else:
+            messages.error(request, 'Ошибка при оформлении заказа. Пожалуйста, проверьте введенные данные.')
+    else:
+        form = OrderForm()
+
+    return render(request, 'checkout.html', {'form': form, 'flower': flower})

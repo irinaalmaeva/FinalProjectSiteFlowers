8218876348b5f3043b34c75c25ddd82c2612c15d
irinaalmaeva <almaeva.irina@gmail.com>
2024-09-19 22:37:58 +0300

    Update urls.py

--- flower_delivery/flowers/urls.py
+++ flower_delivery/flowers/urls.py
@@ -5,4 +5,10 @@ urlpatterns = [
     path('', views.flower_catalog, name='flower_catalog'),
     path('place_order/<int:flower_id>/', views.place_order, name='place_order'),
     path('flower_detail/<int:pk>/', views.flower_detail, name='flower_detail'),
+
+    # Новый маршрут для добавления товара в корзину
+    path('add_to_cart/<int:flower_id>/', views.add_to_cart, name='add_to_cart'),
+
+    # Новый маршрут для просмотра корзины
+    path('cart/', views.view_cart, name='view_cart'),
 ]

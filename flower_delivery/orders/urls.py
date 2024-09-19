3a95f56522d3d71e146d9e7043eef3d3b5d1c27d
irinaalmaeva <almaeva.irina@gmail.com>
2024-09-19 21:45:10 +0300

    update order_history, orders\urls

--- flower_delivery/orders/urls.py
+++ flower_delivery/orders/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('place_order/<int:flower_id>/', views.place_order, name='place_order'),
     path('order_history/', views.order_history, name='order_history'),
     path('order/success/', views.order_success, name='order_success'),
-    path('order_detail/<int:pk>/', views.order_detail, name='order_detail'),
+    path('order_detail/<int:pk>/', views.order_detail, name='order_detail'), # Для отображения подробной информации о заказе
     path('login/', auth_views.LoginView.as_view(), name='login'),  # Используем стандартное представление для входа,
 ]

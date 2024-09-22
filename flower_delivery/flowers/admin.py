1147adb15e2a3b1101c3dc6bac5187e7e6ae09fe
irinaalmaeva <almaeva.irina@gmail.com>
2024-09-22 21:15:18 +0300

    Update admin.py

--- flower_delivery/flowers/admin.py
+++ flower_delivery/flowers/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import Flower
+from orders.models import Order
 
 @admin.register(Flower)
 class FlowerAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'description')
     search_fields = ('name', 'description')
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'status', 'order_date', 'address')
+    filter_horizontal = ('flowers',)  # Для удобного выбора цветов в заказе

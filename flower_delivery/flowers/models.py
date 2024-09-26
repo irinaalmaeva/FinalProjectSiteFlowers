854f4214ff75f4b7cadbc57097e08a431fa5f9d5
irinaalmaeva <almaeva.irina@gmail.com>
2024-09-26 22:15:21 +0300

    Update models.py

--- flower_delivery/flowers/models.py
+++ flower_delivery/flowers/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-
 class Flower(models.Model):
     name = models.CharField(max_length=100, verbose_name='Название')
     description = models.TextField(verbose_name='Описание')

6221a9dbba414a72e71debe100df4184abfbeeff
irinaalmaeva <almaeva.irina@gmail.com>
2024-10-02 21:55:16 +0300

    Update admin.py
    Добавлена возможность изменять статусы заказов администратору

--- flower_delivery/flowers/admin.py
+++ flower_delivery/flowers/admin.py
@@ -11,3 +11,5 @@ class FlowerAdmin(admin.ModelAdmin):
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'status', 'order_date', 'address')
     filter_horizontal = ('flowers',)  # Для удобного выбора цветов в заказе
+    list_filter = ('status',)  # Добавим фильтр по статусу для удобства
+    list_editable = ('status',)  # Позволяет редактировать статус прямо из списка заказов
